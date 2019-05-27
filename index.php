510d3059e522b52c507b7250764aa8efa2aeeade
Hani Ramadhan <hani.its.042@gmail.com>
2019-05-27 16:56:01 +0900

    Update index.php

--- index.php
+++ index.php
@@ -6,8 +6,8 @@
 <h1>Halo</h1>
 <p>Masukkan nama dan nomor telepon Saudara untuk mengisi buku telepon saya! Lalu, klik  <strong>Kirim</strong> untuk menyimpan.</p>
 <form method="post" action="index.php">
-       Nama <input type="text" name="name" id="nama"/></br></br>
-       No. Ponsel <input type="text" name="phone" id="ponsel"/></br></br>
+       Nama <input type="text" name="name" id="name"/></br></br>
+       No. Ponsel <input type="text" name="phone" id="phone"/></br></br>
        <input type="submit" name="submit" value="Submit" />
        <!--<input type="submit" name="load_data" value="Load Data" />-->
  </form>
@@ -26,52 +26,47 @@
         //$conn->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
     } catch(Exception $e) {
         echo "Failed: " . $e;
-    }/*
-    if (isset($_POST['submit'])) {
-        try {
-            $name = $_POST['name'];
-            $email = $_POST['email'];
-            $job = $_POST['job'];
-            $date = date("Y-m-d");
-            // Insert data
-            $sql_insert = "INSERT INTO Registration (name, email, job, date) 
-                        VALUES (?,?,?,?)";
-            $stmt = $conn->prepare($sql_insert);
-            $stmt->bindValue(1, $name);
-            $stmt->bindValue(2, $email);
-            $stmt->bindValue(3, $job);
-            $stmt->bindValue(4, $date);
-            $stmt->execute();
-        } catch(Exception $e) {
-            echo "Failed: " . $e;
-        }
-        echo "<h3>Your're registered!</h3>";
-    } else if (isset($_POST['load_data'])) {
-        try {
-            $sql_select = "SELECT * FROM Registration";
-            $stmt = $conn->query($sql_select);
-            $registrants = $stmt->fetchAll(); 
-            if(count($registrants) > 0) {
-                echo "<h2>People who are registered:</h2>";
-                echo "<table>";
-                echo "<tr><th>Name</th>";
-                echo "<th>Email</th>";
-                echo "<th>Job</th>";
-                echo "<th>Date</th></tr>";
-                foreach($registrants as $registrant) {
-                    echo "<tr><td>".$registrant['name']."</td>";
-                    echo "<td>".$registrant['email']."</td>";
-                    echo "<td>".$registrant['job']."</td>";
-                    echo "<td>".$registrant['date']."</td></tr>";
-                }
-                echo "</table>";
-            } else {
-                echo "<h3>No one is currently registered.</h3>";
+    }
+    try {
+    	$sql_select = "SELECT * FROM PHONEBOOK";
+        $stmt = $conn->query($sql_select);
+        $book = $stmt->fetchAll(); 
+        $pid = count($book);
+
+        $name = $_POST['nama']; 
+        $phone = $_POST['phone'];
+        // Insert data
+        $sql_insert = "INSERT INTO PHONEBOOK (pid, name, phone) 
+                    VALUES (?,?,?)";
+        $stmt = $conn->prepare($sql_insert);
+        $stmt->bindValue(1, $pid);
+        $stmt->bindValue(2, $name);
+        $stmt->bindValue(3, $phone);
+        $stmt->execute();
+   		echo "<h3>Data kontak Anda tersimpan dengan baik!</h3>";
+
+    	$sql_select = "SELECT * FROM PHONEBOOK";
+        $stmt = $conn->query($sql_select);
+        $e = $stmt->fetchAll(); 
+        $book = $stmt->fetchAll(); 
+        if(count($book) > 0) {
+            echo "<h2>Kontak Anda:</h2>";
+            echo "<table>";
+            echo "<tr><th>Nama</th>";
+            echo "<th>No. Telepon</th></tr>";
+            foreach($contact as $book) {
+                echo "<tr><td>".$contact['name']."</td>";
+                echo "<td>".$contact['phone']."</td></tr>";
             }
-        } catch(Exception $e) {
-            echo "Failed: " . $e;
+            echo "</table>";
+        } else {
+            echo "<h3>No one is currently registered.</h3>";
         }
-    }*/
+
+
+    } catch(Exception $e) {
+        echo "Failed: " . $e;
+    }
  ?>
  </body>
  </html>
