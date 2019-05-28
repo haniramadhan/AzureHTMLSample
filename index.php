c65d65851b8c4eaaa078b739f1bc75ce11b8a53f
Hani Ramadhan <hani.its.042@gmail.com>
2019-05-28 11:54:36 +0900

    Update index.php

--- index.php
+++ index.php
@@ -8,7 +8,7 @@
 <form method="post" action="index.php">
        Nama <input type="text" name="name" id="name"/></br></br>
        No. Ponsel <input type="text" name="phone" id="phone"/></br></br>
-       <input type="submit" name="submit" value="Submit" />
+       <input type="submit" name="submit" value="Kirim" />
        <!--<input type="submit" name="load_data" value="Load Data" />-->
  </form>
  <?php
@@ -31,6 +31,43 @@
 		}
     } catch(Exception $e) {
         echo "Failed: " . $e;
+    } try {	
+    	$sql_select = "SELECT * FROM PHONEBOOK";	
+        $stmt = $conn->query($sql_select);	
+        $book = $stmt->fetchAll(); 	
+        $pid = count($book);	
+         $name = $_POST['nama']; 	
+        $phone = $_POST['phone'];	
+        // Insert data	
+        /*
+        $sql_insert = "INSERT INTO PHONEBOOK (pid, name, phone) 	
+                    VALUES (?,?,?)";	
+        $stmt = $conn->prepare($sql_insert);	
+        $stmt->bindValue(1, $pid);	
+        $stmt->bindValue(2, $name);	
+        $stmt->bindValue(3, $phone);	
+        $stmt->execute();	
+        */
+   		echo "<h3>Data kontak Anda tersimpan dengan baik!</h3>";	
+     	$sql_select = "SELECT * FROM PHONEBOOK";	
+        $stmt = $conn->query($sql_select);	
+        $e = $stmt->fetchAll(); 	
+        $book = $stmt->fetchAll(); 	
+        if(count($book) > 0) {	
+            echo "<h2>Kontak Anda:</h2>";	
+            echo "<table>";	
+            echo "<tr><th>Nama</th>";	
+            echo "<th>No. Telepon</th></tr>";	
+            foreach($contact as $book) {	
+                echo "<tr><td>".$contact['name']."</td>";	
+                echo "<td>".$contact['phone']."</td></tr>";	
+            }	
+            echo "</table>";	
+        } else {	
+            echo "<h3>No one is currently registered.</h3>";	
+        }	
+     } catch(Exception $e) {	
+        echo "Failed: " . $e;	
     }
  ?>
  </body>
