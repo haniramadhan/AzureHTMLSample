894574b807d66f70db5179cf8ecfbb45c348a989
Hani Ramadhan <hani.its.042@gmail.com>
2019-05-28 14:52:27 +0900

    Update index.php

--- index.php
+++ index.php
@@ -24,51 +24,49 @@
     try {
         $conn = new PDO("sqlsrv:server = $host; Database = $db", $user, $pass);
         $conn->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
-  //       $conn = sqlsrv_connect($host, $connectionOptions);
-  //       if($conn === false)
-		// {
-		//     die(print_r(sqlsrv_errors(), true));
-		// }
+
+
+        $sql_select = "SELECT * FROM PHONEBOOK";    
+        $stmt = $conn->query($sql_select);  
+        $book = $stmt->fetchAll();  
+        if(count($book) > 0) {  
+            echo "<h2>Kontak Anda:</h2>";   
+            echo "<table>"; 
+            echo "<tr><th>Nama</th>";   
+            echo "<th>No. Telepon</th></tr>";   
+            foreach($contact as $book) {    
+                echo "<tr><td>".$contact['name']."</td>";   
+                echo "<td>".$contact['phone']."</td></tr>"; 
+            }   
+            echo "</table>";    
+        } else {    
+            echo "<h3>No one is currently registered.</h3>";    
+        }     
+        $pid = count($book);    
+        $name = $_POST['nama'];     
+        $phone = $_POST['phone']; 
     } catch(Exception $e) {
         echo "Failed: " . $e;
-    } try {	
-    	$sql_select = "SELECT * FROM PHONEBOOK";	
-        $stmt = $conn->query($sql_select);	
-        $book = $stmt->fetchAll(); 	
-        $pid = count($book);	
-        $name = $_POST['nama']; 	
-        $phone = $_POST['phone'];	
-        // Insert data	
-        /*
-        $sql_insert = "INSERT INTO PHONEBOOK (pid, name, phone) 	
-                    VALUES (?,?,?)";	
-        $stmt = $conn->prepare($sql_insert);	
-        $stmt->bindValue(1, $pid);	
-        $stmt->bindValue(2, $name);	
-        $stmt->bindValue(3, $phone);	
-        $stmt->execute();	
-        */
-   		echo "<h3>Data kontak Anda tersimpan dengan baik!</h3>";	
-     	$sql_select = "SELECT * FROM PHONEBOOK";	
-        $stmt = $conn->query($sql_select);	
-        $e = $stmt->fetchAll(); 	
-        $book = $stmt->fetchAll(); 	
-        if(count($book) > 0) {	
-            echo "<h2>Kontak Anda:</h2>";	
-            echo "<table>";	
-            echo "<tr><th>Nama</th>";	
-            echo "<th>No. Telepon</th></tr>";	
-            foreach($contact as $book) {	
-                echo "<tr><td>".$contact['name']."</td>";	
-                echo "<td>".$contact['phone']."</td></tr>";	
-            }	
-            echo "</table>";	
-        } else {	
-            echo "<h3>No one is currently registered.</h3>";	
-        }	
-     } catch(Exception $e) {	
-        echo "Failed: " . $e;	
     }
+
+    if (isset($_POST['submit'])) {
+        try {	
+            // Insert data	
+            
+            $sql_insert = "INSERT INTO PHONEBOOK (pid, name, phone) 	
+                        VALUES (?,?,?)";	
+            $stmt = $conn->prepare($sql_insert);	
+            $stmt->bindValue(1, $pid);	
+            $stmt->bindValue(2, $name);	
+            $stmt->bindValue(3, $phone);	
+            $stmt->execute();	
+            
+            
+         } catch(Exception $e) {	
+            echo "Failed: " . $e;	
+        }
+    }
+
  ?>
  </body>
  </html>
